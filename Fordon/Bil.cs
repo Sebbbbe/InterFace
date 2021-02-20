368205026961d3c6ed83e6761d2f1759001e1ea0
Sebbbbe <sebastian.tiger901@gmail.com>
2021-02-20 15:18:39 +0100

    bil-klar

---
+++ Fordon/Bil.cs
@@ -0,0 +1,27 @@
+﻿using farkost.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace farkost.Fordon
+{
+    public class Bil : IFarkostModel
+    {
+ 
+        public Bil()
+        {
+
+        }
+        public string FarkostTyp { get; set; }
+        public string FarkostNamn { get; set; }
+        public int MaxHastighet { get; set; }
+        public string Märke { get; set; }
+        public string ModelName { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public int HurMångaHjul { get; set; }
+    }
+}
---
+++ Interface/IFarkostModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace farkost.Interface
+{
+   public interface IFarkostModel
+    {
+        string FarkostTyp { get; set; }
+         string FarkostNamn { get; set; }
+        string Märke { get; set; }
+        string ModelName { get; set; }
+        int MaxHastighet { get; set; }
+       
+      
+        int ReleaseYear { get; set; }
+    }
+}
